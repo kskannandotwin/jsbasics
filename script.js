8eadd468294061bfe54f07b718e7996c90f4cd94
kskannandotwin <kskannan.win@gmail.com>
2023-11-17 19:26:58 +0530

    17.11.2023-bl
    completed

--- script.js
+++ script.js
@@ -157,7 +157,20 @@ if (isMarried) {
 } else {
     console.log(firstName + ' will hopefully marry soon!');
 }
- 
+
+// boolean logic
+var firstName = 'John';
+var age = 20;
+
+if (age < 13) {
+    console.log(firstName + ' is a boy');
+} else if (age >= 13 && age < 20) {
+    console.log(firstName + ' is a teenager');
+} else if (age >= 20 && age < 30) {
+    console.log(firstName + ' is a young man');
+} else {
+    console.log(firstName + ' is a man');
+}
 
 /*****************************
 * Boolean logic
