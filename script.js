72cd5ff0abffc9e741321430fead21e6342764be
kskannandotwin <kskannan.win@gmail.com>
2023-11-22 06:26:29 +0530

    22.11.2023-cc-2
    completed

--- script.js
+++ script.js
@@ -199,7 +199,7 @@ switch (job) {
     // console.log(firstName + " designs beautiful websites");
     break;
   default:
-    // console.log(firstName + " does something else");
+  // console.log(firstName + " does something else");
 }
 
 // truthy and falsy values and equality operators
@@ -211,59 +211,44 @@ var height;
 height = 23;
 
 if (height || height === 0) {
-  console.log("variable is defined");
+  //   console.log("variable is defined");
 } else {
-  console.log("variable has not been defined");
+  //   console.log("variable has not been defined");
 }
 
 // equality operators
-if (height === '23') {
-    console.log('The == operator does type coercion!');
+if (height === "23") {
+  //   console.log("The == operator does type coercion!");
 } else {
-    console.log('The == operator does note type coercion!');
+  //   console.log("The == operator does note type coercion!");
 }
 
-/*****************************
- * CODING CHALLENGE 2
- */
-
-/*
-John and Mike both play basketball in different teams. In the latest 3 games, John's team scored 89, 120 and 103 points, while Mike's team scored 116, 94 and 123 points.
-
-1. Calculate the average score for each team
-2. Decide which teams wins in average (highest average score), and print the winner to the console. Also include the average score in the output.
-3. Then change the scores to show different winners. Don't forget to take into account there might be a draw (the same average score)
-
-4. EXTRA: Mary also plays basketball, and her team scored 97, 134 and 105 points. Like before, log the average winner to the console. HINT: you will need the && operator to take the decision. If you can't solve this one, just watch the solution, it's no problem :)
-5. Like before, change the scores to generate different winners, keeping in mind there might be draws.
-
-GOOD LUCK 😀
-*/
-/*
+// coding challenge 2
 var scoreJohn = (189 + 120 + 103) / 3;
 var scoreMike = (129 + 94 + 123) / 3;
-var scoreMary = (97 + 134 + 105) / 3;
-console.log(scoreJohn, scoreMike, scoreMary);
+var scoreMary = (97 + 134 + 103) / 3;
+// console.log(scoreJohn, scoreMike, scoreMary);
 
 if (scoreJohn > scoreMike && scoreJohn > scoreMary) {
-    console.log('John\'s team wins with ' + scoreJohn + ' points');
-} else if (scoreMike > scoreJohn && scoreMike > scoreMary) {
-    console.log('Mike\'s team wins with ' + scoreMike + ' points');
-} else if (scoreMary > scoreJohn && scoreMary > scoreMike) {
-    console.log('Mary\'s team wins with ' + scoreMary + ' points');
-} else {
-    console.log('There is a draw');
-}
+    // console.log("John's team wins with " + scoreJohn + " points");
+  } else if (scoreMike > scoreJohn && scoreMike > scoreMary) {
+    // console.log("Mike's team wins with " + scoreMike + " points");
+  }else if (scoreMary > scoreJohn && scoreMary > scoreMike) {
+    // console.log("Mary's team wins with " + scoreMary + " points");
+  } else {
+    // console.log("There is a draw");
+  }
 
-
+/*
 if (scoreJohn > scoreMike) {
-    console.log('John\'s team wins with ' + scoreJohn + ' points');
+  console.log("John's team wins with " + scoreJohn + " points");
 } else if (scoreMike > scoreJohn) {
-    console.log('Mike\'s team wins with ' + scoreMike + ' points');
+  console.log("Mike's team wins with " + scoreMike + " points");
 } else {
-    console.log('There is a draw');
+  console.log("There is a draw");
 }
 */
+
 
 /*****************************
  * Functions
