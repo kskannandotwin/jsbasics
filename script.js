27a35c94a964ae9271ba9fbc269c84240d6f554f
kskannandotwin <kskannan.win@gmail.com>
2024-01-18 20:00:43 +0530

    18.01.2024-cc5
    completed

--- script.js
+++ script.js
@@ -1,40 +1,83 @@
-// for loop
-for (var i = 1; i <= 20; i += 2) {
-  console.log(i);
+var john = {
+  fullName: "John Smith",
+  bills: [124, 48, 268, 180, 42],
+  calcTips: function () {
+    this.tips = [];
+    this.finalValues = [];
+
+    for (var i = 0; i < this.bills.length; i++) {
+      // Determine percentage based on tipping rules
+      var percentage;
+      var bill = this.bills[i];
+
+      if (bill < 50) {
+        percentage = 0.2;
+      } else if (bill >= 50 && bill < 200) {
+        percentage = 0.15;
+      } else {
+        percentage = 0.1;
+      }
+
+      // Add results to the corresponing arrays
+      this.tips[i] = bill * percentage;
+      this.finalValues[i] = bill + bill * percentage;
+    }
+  },
+};
+
+var mark = {
+  fullName: "Mark Miller",
+  bills: [77, 475, 110, 45],
+  calcTips: function () {
+    this.tips = [];
+    this.finalValues = [];
+
+    for (var i = 0; i < this.bills.length; i++) {
+      // Determine percentage based on tipping rules
+      var percentage;
+      var bill = this.bills[i];
+
+      if (bill < 100) {
+        percentage = 0.2;
+      } else if (bill >= 100 && bill < 300) {
+        percentage = 0.1;
+      } else {
+        percentage = 0.25;
+      }
+
+      // Add results to the corresponing arrays
+      this.tips[i] = bill * percentage;
+      this.finalValues[i] = bill + bill * percentage;
+    }
+  },
+};
+
+function calcAverage(tips) {
+  var sum = 0;
+  for (var i = 0; i < tips.length; i++) {
+    sum = sum + tips[i];
+  }
+  return sum / tips.length;
 }
 
-// i = 0, 0 < 10 true, log i to console, i++
-// i = 1, 1 < 10 true, log i to the console, i++
-//...
-// i = 9, 9 < 10 true, log i to the console, i++
-// i = 10, 10 < 10 FALSE, exit the loop!
+// Do the calculations
+john.calcTips();
+mark.calcTips();
 
-var john = ["John", "Smith", 1990, "designer", false, "blue"];
-for (var i = 0; i < john.length; i++) {
-  console.log(john[i]);
-}
+john.average = calcAverage(john.tips);
+mark.average = calcAverage(mark.tips);
+console.log(john, mark);
 
-// While loop
-var i = 0;
-while (i < john.length) {
-  console.log(john[i]);
-  i++;
-}
-
-// continue and break statements
-var john = ["John", "Smith", 1990, "designer", false, "blue"];
-
-for (var i = 0; i < john.length; i++) {
-  if (typeof john[i] !== "string") continue;
-  console.log(john[i]);
-}
-
-for (var i = 0; i < john.length; i++) {
-  if (typeof john[i] !== "string") break;
-  console.log(john[i]);
-}
-
-// Looping backwards
-for (var i = john.length - 1; i >= 0; i--) {
-  console.log(john[i]);
+if (john.average > mark.average) {
+  console.log(
+    john.fullName +
+      "'s family pays higher tips, with an average of $" +
+      john.average
+  );
+} else if (mark.average > john.average) {
+  console.log(
+    mark.fullName +
+      "'s family pays higher tips, with an average of $" +
+      mark.average
+  );
 }
