06acba2d0c4d7f1f29cbcf909bba319afa307823
kskannandotwin <kskannan.win@gmail.com>
2024-01-18 19:32:44 +0530

    18.01.2024-cc4
    completed

--- script.js
+++ script.js
@@ -1,14 +1,27 @@
 var john = {
-  firstName: "John",
-  lastName: "Smith",
-  birthYear: 1992,
-  family: ["Jane", "Mark", "Bob", "Emily"],
-  job: "teacher",
-  isMarried: false,
-  calcAge: function () {
-    this.age = 2018 - this.birthYear;
+  fullName: "John Smith",
+  mass: 110,
+  height: 1.95,
+  calcBMI: function () {
+    this.bmi = this.mass / (this.height * this.height);
+    return this.bmi;
   },
 };
 
-john.calcAge();
-console.log(john);
+var mark = {
+  fullName: "Mark Miller",
+  mass: 78,
+  height: 1.69,
+  calcBMI: function () {
+    this.bmi = this.mass / (this.height * this.height);
+    return this.bmi;
+  },
+};
+
+if (john.calcBMI() > mark.calcBMI()) {
+  console.log(john.fullName + " has a higher BMI of " + john.bmi);
+} else if (mark.bmi > john.bmi) {
+  console.log(mark.fullName + " has a higher BMI of " + mark.bmi);
+} else {
+  console.log("They have the same BMI");
+}
