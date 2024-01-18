16d4eb09e9286ff29a52b8745f30feccd5e81a32
kskannandotwin <kskannan.win@gmail.com>
2024-01-18 19:30:41 +0530

    18.01.2024-oam
    completed

--- script.js
+++ script.js
@@ -1,25 +1,14 @@
-// Object literal
 var john = {
   firstName: "John",
   lastName: "Smith",
-  birthYear: 1990,
+  birthYear: 1992,
   family: ["Jane", "Mark", "Bob", "Emily"],
   job: "teacher",
   isMarried: false,
+  calcAge: function () {
+    this.age = 2018 - this.birthYear;
+  },
 };
 
-console.log(john.firstName);
-console.log(john["lastName"]);
-var x = "birthYear";
-console.log(john[x]);
-
-john.job = "designer";
-john["isMarried"] = true;
+john.calcAge();
 console.log(john);
-
-// new Object syntax
-var jane = new Object();
-jane.firstName = "Jane";
-jane.birthYear = 1969;
-jane["lastName"] = "Smith";
-console.log(jane);
